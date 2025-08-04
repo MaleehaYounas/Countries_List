93306d514dda40b0e0615e7e21001034fc31ed57
MaleehaYounas <maleeha139@gmail.com>
2025-08-04 12:20:37 +0500

    Added Search and Sort

---
+++ Countries/Models/NetworkModel.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkModel.swift
+//  Countries
+//
+//  Created by MAC on 01/08/2025.
+//
+
+import SwiftUI
+
+struct NetworkModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkModel()
+}
